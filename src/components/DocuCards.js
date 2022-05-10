89f3fd26010685e1782b6e17e0d489fa42f7b704
Kate Montgomery <katemontgomery431@gmail.com>
2022-05-10 12:20:10 -0400

    title page movie covers

--- src/components/DocuCards.js
+++ src/components/DocuCards.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 function DocuCards() {
+    const { id, title, comment, rating, where, image } = docsInfo;
+
+    function renderInfo() {
+      fetch(`http://localhost:3001/natureDocs/`)
+        .then(res => res.json())
+        .then(info => loadInfo(info));
+    }
+
   return (
     <div className="card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{""}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={""}
+        alt={""}
         className=""
       />
       <p>{""} Likes </p>
@@ -14,8 +22,7 @@ function DocuCards() {
       <button className="del-btn"></button>
     </div>
   );
-}
-
+  }
 
 
 
