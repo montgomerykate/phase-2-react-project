45053a73fa8cb17c17d6f39f438ce968b7f1c891
Kate Montgomery <katemontgomery431@gmail.com>
2022-05-11 11:09:05 -0400

    trailers.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { BrowserRouter, route } from "react-router-dom";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
+
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
