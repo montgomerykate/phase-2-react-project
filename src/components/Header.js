564866513b423a9d095d6a5c00778029c970f363
Kate Montgomery <katemontgomery431@gmail.com>
2022-05-12 22:33:15 -0400

    How neat is that

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,15 +6,16 @@ function Header({ search, handleSearch }) {
   return (
     <header>
       <h1>
-        NatureTube
+        NEATureTube: 
       </h1>
+      <h2> How neat is that</h2>
       <Search search={search} handleSearch={handleSearch}/>
       <nav>
         <ul>
           <li>
             <Link to= "/">Home</Link>
             </li>
-            
+
             <li>
             <Link to="/newdocform">Add Documentary</Link>
             </li>
