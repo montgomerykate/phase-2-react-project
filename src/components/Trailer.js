6754d21b39c7b4e84caef38d9424fa68b854aea0
Kate Montgomery <katemontgomery431@gmail.com>
2022-05-12 21:56:41 -0400

    youtube WORKS

--- src/components/Trailer.js
+++ src/components/Trailer.js
@@ -1,22 +1,21 @@
 import React from "react";
 import { useRouteMatch } from "react-router-dom";
 
-const Trailer = (props) => {
-    const match = useRouteMatch(); // allows you to access the rounterProps match obj, which hold the slug / dynamic varibale from the url
+const Trailer = () => {
+    const match = useRouteMatch();
     const embedId = match.params.embedId
     
     return (
     <div className="video-responsive">
-        <h1>MovieTrailer-- embedId{embedId}</h1> {/* placeholder */}
-        <iframe
-      width="853"
-      height="480"
-      src={`https://www.youtube.com/embed/${embedId}`}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      title="Embedded youtube"
-    />
+        <iframe 
+        width="560" 
+        height="315" 
+        src= {`https://www.youtube.com/embed/${embedId}`}
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen>
+        </iframe>
     </div>
     )
 
